925a873e73b706f3a442c876e697affcd6726297
chenshuang <cs123@qq.com>
2018-08-31 10:46:03 +0800

    修改报价识别

--- src/components/Echart.vue
+++ src/components/Echart.vue
@@ -148,7 +148,7 @@
             <textarea class="row" id="groupHairMsg" name="groupHairMsg" v-model="groupMsg" placeholder="请您粘贴信息"></textarea>
           </form>
         </div>
-        <div class="c_shiBieButton">
+        <div class="c_shiBieButton" data-cnt="0" ref="btn" @click="showShibie($event)" id='quick_parse_create'>
           报价识别
         </div>
         <!--<div class="vu_fenzu_footer">-->
@@ -335,6 +335,202 @@
       },
       closeQunFa: function () {
         this.showQunFa = false;
+      },
+      showShibie: function (event) {
+        var _parse_txt_result = false;
+        var $_this = $(event.currentTarget);
+        console.log('data.cnt:',$_this.data('cnt'));
+        var opt ={
+          pagination: false,
+          clickToSelect: true,
+          dataType: "json",
+          method: 'POST',
+          sidePagination: 'server',
+          pageSize: 200,  //200条记录应该是足够了
+          pageNumber: 1,
+          url: 'http://www.energy.cn/match/offer/create',
+          contentType: "application/x-www-form-urlencoded",
+          queryParams: function (param) {
+            //追加额外的请求参数
+            param.opt = 'quick_parse';
+            param.catalog_id = match_hall_catalogs[0].catalog_id;
+            param.import_txt = $('#quick_parse_txt').val();
+            return param;
+          },
+          responseHandler: function(res) {
+            if (res.code == 0) {
+              _parse_txt_result = false;
+              //解析失败,若已打开群发按钮，仍需群发消息
+              if(false){//_is_auto_hair
+                $('#quick_parse_submit_price').trigger('click');
+              }else{
+                //不需要群发
+                $("#quick_parse_new").hide();
+                // toastr.warning(res.msg);
+                return false;
+              }
+            }else{
+              _parse_txt_result = true;
+              alert('show');
+              $("#quick_parse_new").show();
+              return res;
+            }
+          },
+          onLoadSuccess:function(res){
+            if(res.code == 1){
+              for(var i=0,lgth=res.data.length;i<lgth;i++){
+                var index = i;
+                //如果价格为空,重新样式
+                if(parseInt(res.data[i].trade_price)<1){
+                  $_tr = $('#quick_parse_new_data_table tbody tr').eq(i);
+                  $_tr.css('background-color','#fff8f1');
+                  $_td = $("a[data-name='trade_price']",$_tr).parent();
+                  $_td.css('border','1px solid #ff0000');
+                }
+              }
+            }
+          },
+          cache: false,
+          totalField: 'total',
+          dataField: 'data',
+          editable: true,
+          uniqueId: 'index',
+          columns: [{
+            field: "product_id",
+            title: "品类",
+            visible: false,
+            editable: {
+              type: 'text',
+              title: '品类',
+              validate: function (v) {
+                if (!v) return '品类不能为空';
+
+              }
+            }
+          }, {
+            field: "index",
+            title: "编号",
+          },{
+            field: "trade_type",
+            title: "类型",
+            editable: {
+              type: 'select',
+              source: [{ value: 1, text: "买" }, { value: -1, text: "卖" }, {value:2,text:"成交"}],
+              title: '类型',
+              validate: function (v) {
+                if (!v) return '类型不能为空';
+
+              }
+            }
+          }, {
+            field: "trade_price",
+            title: "价格",
+            editable: {
+              type: 'text',
+              title: '价格',
+              validate: function (v) {
+                if (!v) return '价格不能为空';
+              }
+            }
+          }, {
+            field: "trade_amount",
+            title: "数量",
+            editable: {
+              type: 'text',
+              title: '数量',
+              validate: function (v) {
+                if (!v) return '数量不能为空';
+              }
+            }
+          },{
+            field: "bond",
+            title: "保证金",
+            editable: {
+              type: 'select',
+              source:[{ value: "0", text: "0保" },{ value: "1", text: "5%" },{ value: "2", text: "免保" }],
+              title: '保证金',
+            }
+          },{
+            field: "trader_id",
+            title: "交易商",
+            editable: {
+              type: 'select',
+              source:[{value:"0",text:"无"},{value:"81",text:"国投"},{value:"83",text:"金诺"},{value:"84",text:"国企"},{value:"85",text:"浙江金龙贸易发展有限公司"},{value:"86",text:"浙江尤夫高新纤维股份有限公司"},{value:"87",text:"中国国投国际贸易南京有限公司"},{value:"88",text:"福建联合石油化工有限公司"},{value:"89",text:"宁波源涌贸易有限公司"},{value:"90",text:"宁波富德能源有限公司"},{value:"91",text:"厦门荣锦贸易有限公司"},{value:"92",text:"中基宁波集团股份有限公司"},{value:"93",text:"恒力石化（大连）有限公司"},{value:"94",text:"中铝国际工程设备有限公司"},{value:"95",text:"华祥（中国）高纤有限公司"},{value:"96",text:"江苏海欣纤维有限公司"},{value:"97",text:"衡都"},{value:"101",text:"摩贝"},{value:"103",text:"世能"},{value:"107",text:"亨承"},{value:"108",text:"博纳"},{value:"109",text:"华润"},{value:"110",text:"前程"},{value:"111",text:"东越"},{value:"112",text:"醒狮"},{value:"113",text:"主流"},{value:"114",text:"港联"},{value:"115",text:"凯文"},{value:"116",text:"昱旸"},{value:"118",text:"安徽祯之信"},{value:"119",text:"海鹏"},{value:"120",text:"恒逸"},{value:"121",text:"隆焻"},{value:"122",text:"良恩"},{value:"123",text:"光华"},{value:"124",text:"盛楼/创元"},{value:"125",text:"畅一骁"},{value:"126",text:"沃娇"},{value:"127",text:"天津港通"},{value:"128",text:"华庆"},{value:"129",text:"东辰"},{value:"130",text:"朋荣"},{value:"131",text:"中南"},{value:"134",text:"金隆"},{value:"135",text:"换手盘"},{value:"136",text:"振辉"},{value:"137",text:"乐丰"},{value:"138",text:"塑盛"},{value:"139",text:"中和"},{value:"140",text:"泰地"},{value:"141",text:"巽融"},{value:"142",text:"龙运"},{value:"143",text:"远大"},{value:"144",text:"正一"},{value:"145",text:"苏克"},{value:"146",text:"上海新天"},{value:"147",text:"精功换手"},{value:"148",text:"中商"},{value:"149",text:"凯富龙"},{value:"150",text:"锦壬"},{value:"151",text:"铭源物产"},{value:"152",text:"静安"},{value:"153",text:"工厂"},{value:"154",text:"中商沪深"},{value:"155",text:"浙江远能"},{value:"156",text:"嘉塑盛"},{value:"157",text:"金桥"},{value:"158",text:"四邦"},{value:"159",text:"华君"},{value:"160",text:"江苏徽熳"},{value:"161",text:"上海韵彬"},{value:"162",text:"青岛步天"},{value:"163",text:"常州博昊"},{value:"164",text:"华茂"},{value:"165",text:"茂煊"},{value:"166",text:"泰地"},{value:"167",text:"辛逸"},{value:"168",text:"前程"},{value:"169",text:"远大能源"},{value:"170",text:"联弘"},{value:"171",text:"香塘"},{value:"172",text:"新凤鸣"},{value:"173",text:"万凯"},{value:"174",text:"三房巷"},{value:"175",text:"金桥"},{value:"176",text:"群星"},{value:"177",text:"延长"},{value:"178",text:"方正"},{value:"179",text:"汇鸿"},{value:"180",text:"春风"},{value:"181",text:"南通化轻"},{value:"182",text:"长城"},{value:"183",text:"象屿"},{value:"184",text:"国贸启润"},{value:"185",text:"中化塑"},{value:"186",text:"浙江物产"},{value:"187",text:"中铝"},{value:"188",text:"国投"},{value:"189",text:"国投张家港"},{value:"190",text:"中船重工"},{value:"191",text:"灏润"},{value:"192",text:"醒狮"},{value:"193",text:"五洋"},{value:"194",text:"百地年"},{value:"195",text:"苏南"},{value:"196",text:"精功"},{value:"197",text:"银亿"},{value:"198",text:"巨雄"},{value:"199",text:"远大石化"},{value:"200",text:"华升"},{value:"201",text:"正凯"},{value:"202",text:"世能"},{value:"203",text:"恒杰"},{value:"204",text:"正杰"},{value:"205",text:"嘉塑盛贸"},{value:"206",text:"仕进"},{value:"207",text:"孚豪"},{value:"208",text:"卓泰"},{value:"209",text:"平衡"},{value:"210",text:"鼎衡"},{value:"211",text:"凯越"},{value:"212",text:"祯之信"},{value:"213",text:"杭实善成"},{value:"218",text:"前程"},{value:"234",text:"国贸启润"},{value:"332",text:"test陈以权"},{value:"333",text:"日出"},{value:"334",text:"康暄"},{value:"335",text:"奥文"},{value:"336",text:"皮塑"},{value:"337",text:"精业新材料"},{value:"338",text:"上海柄辰"},{value:"339",text:"东裕"},{value:"340",text:"浙江经世"},{value:"341",text:"勤元运生"},{value:"342",text:"赞成"},{value:"343",text:"创赢"},{value:"344",text:"江苏科恩"},{value:"345",text:"智传"},{value:"346",text:"宁波东菱"},{value:"347",text:"塑来"},{value:"348",text:"恒祺"},{value:"349",text:"中同"},{value:"350",text:"中瑞"},{value:"351",text:"商州"},],
+              title: '交易商',
+            }
+          }, {
+            field: "stock",
+            title: "仓库",
+            editable: {
+              type: 'select',
+              source:[{value: "2", text: "国际" },{value: "9", text: "太仓" },{value: "13", text: "宁波" },{value: "14", text: "常熟" },{value: "15", text: "华西" },{value: "16", text: "乍浦" }],
+              title: '仓库',
+              validate: function (v) {
+                if (!v) return '仓库不能为空';
+
+              }
+            }
+          },{
+            field: "delivery_start",
+            title: "交割起",
+            class:'jiaoge_start',
+            formatter: function (value, row, index) {
+              return value;
+            },
+            editable: {
+              type: 'date',
+              format: "yyyy-mm-dd",
+              title: '开始时间',
+              pickerPosition:"top-left",
+              validate: function (v) {
+                if (!v) return '交割起时间不能为空';
+              }
+            }
+          },{
+            field: "delivery_end",
+            title: "交割止",
+            class:'jiaoge_end',
+            formatter: function (value, row, index) {
+              return value;
+            },
+            editable: {
+              type: 'date',
+              format: "yyyy-mm-dd",
+              title: '截止时间',
+              validate: function (v) {
+                if (!v) return '截止时间不能为空';
+              }
+            }
+          },{
+            field: "raw_data",
+            title: "原始信息",
+            editable: {
+              type: 'text',
+              title: '原始信息',
+              validate: function (v) {
+                if (!v) return '原始信息不能为空';
+
+              }
+            }
+          },{
+            title: "　　操作　　",
+            formatter: function (value, row, index) {
+              var f='<a class="btn btn-danger remove remove_parse" data-id="'+row.index+'" style="color:#fff;">删除</a>&nbsp;';
+              f+= '<a class="btn btn-info remove error_parse" style="color:#fff;" data-note="'+row.raw_data+'" title="交割期解析出错，请上报">报错</a>';
+              return f;
+            },
+          },
+          ],
+        };
+        if(parseInt($_this.data('cnt'))<1){
+          $_this.data('cnt',1);
+          $("#quick_parse_new_data_table").bootstrapTable(opt);
+          console.log(opt)
+          $("#quick_parse_new").show();
+        }else{
+          console.log($_this.data('cnt'));
+          $("#quick_parse_new_data_table").bootstrapTable('refresh',opt);
+        }
       },
       /* qunNew部分结束 */
       kindclick: function (){
