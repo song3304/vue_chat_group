180ce97b97ea5ea3d229c910d0aaf29254371890
ji <2450145403@qq.com>
2018-10-23 16:13:49 +0800

    调样式

--- src/components/chat_dialog/send.vue
+++ src/components/chat_dialog/send.vue
@@ -41,7 +41,7 @@ export default {
 <template>
     <div class="vu_m-text" v-if="session!=null&&session.type!='qun'">
     		<div class="vu_history" title="历史记录" @click="openHistoryDialog(session.id,'user')"><img src="../../images/lishi .png" alt="" /> <span>历史记录</span> </div>
-        <textarea placeholder="" v-model="text" @click="toRead(session)" @keyup.enter="inputing" @keyuo.13="toRead(session)" placeholder="聊一聊"></textarea>
+        <textarea v-model="text" @click="toRead(session)" @keyup.enter="inputing" @keyuo.13="toRead(session)" placeholder="聊一聊"></textarea>
         <span class="vu_send" @click="inputing">发送</span><!--<span class="vu_history" @click="openHistoryDialog(session.id)">历史记录<p></p></span>-->
    </div>
 </template>
