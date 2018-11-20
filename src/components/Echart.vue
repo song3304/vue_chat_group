427092ca3a748e55c22f6e2c8ba81c765b19de2b
ji <2450145403@qq.com>
2018-11-20 18:02:48 +0800

    先隐藏保留切换状态

--- src/components/Echart.vue
+++ src/components/Echart.vue
@@ -625,27 +625,27 @@
 		      minHeight: 464,
 		      minWidth: 562
 	    });
-	    $('.vue-chart-foot .vue-real-img span').removeClass('vue-real-img_active')
-	    if(localStorage.realmain==undefined){
-	    	$('.vue-chart-foot .vue-real-img span').eq(0).addClass('vue-real-img_active')
-	    }else{
-	    	$('.vue-chart-foot .vue-real-img span').eq(localStorage.realmain).addClass('vue-real-img_active')
-	    	if(localStorage.realmain==1){
-		    	this.tealtime_time=false
-					this.tealtime_timecuo=true
-					this.tealtime_timetrade=false
-					this.matchtiao();
-		    }else if(localStorage.realmain==2){
-		    	this.tealtime_time=false
-					this.tealtime_timecuo=false
-					this.tealtime_timetrade=true
-					this.tradetiao();
-		    }else{
-		    	this.tealtime_time=true
-					this.tealtime_timecuo=false
-					this.tealtime_timetrade=false
-		    }
-	    }	    				    	          					
+//	    $('.vue-chart-foot .vue-real-img span').removeClass('vue-real-img_active')
+//	    if(localStorage.realmain==undefined){
+//	    	$('.vue-chart-foot .vue-real-img span').eq(0).addClass('vue-real-img_active')
+//	    }else{
+//	    	$('.vue-chart-foot .vue-real-img span').eq(localStorage.realmain).addClass('vue-real-img_active')
+//	    	if(localStorage.realmain==1){
+//		    	this.tealtime_time=false
+//					this.tealtime_timecuo=true
+//					this.tealtime_timetrade=false
+//					this.matchtiao();
+//		    }else if(localStorage.realmain==2){
+//		    	this.tealtime_time=false
+//					this.tealtime_timecuo=false
+//					this.tealtime_timetrade=true
+//					this.tradetiao();
+//		    }else{
+//		    	this.tealtime_time=true
+//					this.tealtime_timecuo=false
+//					this.tealtime_timetrade=false
+//		    }
+//	    }	    				    	          					
     },
     methods: {
     	saveGroup:function(groupId,groupType,uids){
