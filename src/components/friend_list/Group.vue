d675c4d31005e0f30cdb4d3b7ba4c63ecdcf8c6e
ji <2450145403@qq.com>
2018-11-26 17:06:47 +0800

    增加聊天列表滚动条

--- src/components/friend_list/Group.vue
+++ src/components/friend_list/Group.vue
@@ -89,7 +89,7 @@ export default {
     openChat: function (uid) {
       this.$emit('openChartEvent', uid, 'user')
       $('.vu_m-list').show()
-      $(".vu_m-list ul").niceScroll({
+      $(".vu_m-list ul,.vu_liaotian_left ul").niceScroll({
     	cursorcolor: "#cccccc", // 改变滚动条颜色，使用16进制颜色值
         cursoropacitymax: 1, // 当滚动条是显示状态时改变透明度, 值范围 1 到 0
         cursorwidth: "5px", // 滚动条的宽度，单位：便素
