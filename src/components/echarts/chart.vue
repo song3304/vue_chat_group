999933e13c9f6d5332688d8d6d190ed63fadeb25
ji <2450145403@qq.com>
2019-02-27 10:33:14 +0800

    微调

--- src/components/echarts/chart.vue
+++ src/components/echarts/chart.vue
@@ -420,7 +420,7 @@ export default {
           var year=date.getFullYear();
           var month=date.getMonth()+1;
           if(month>=1 && month<=9){
-              month+='0'+month;
+              month='0'+month;
           }
           var day=date.getDate();
           return year+sep+month+sep+day+' '+'00:00:00';
